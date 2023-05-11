4bab768556a3bb548dc3d3d24d7eb41e725fc5f2
hou-12 <82287928+hou-12@users.noreply.github.com>
2023-05-12 01:34:26 +0200

    Update test-intersection.c

--- test/inversion-list/test-intersection.c
+++ test/inversion-list/test-intersection.c
@@ -20,17 +20,19 @@ int main(void) {
 
     InversionList *set = inversion_list_create(20, sizeof a / sizeof *a, a);
     InversionList *set1;
+    InversionList *null = NULL;
 
     InversionList *set2 = inversion_list_create(20, sizeof b / sizeof *b, b);
     InversionList *set3 = inversion_list_create(20, sizeof c / sizeof *c, c);
 
-    set1 = inversion_list_intersection(set, set2, set3, reinterpret_cast<InversionList*>(NULL));
+    set1 = inversion_list_intersection(set, set2, set3, null);
     assert(strcmp(inversion_list_to_string(set1), "[1, 2, 3, 5, 7, 9]") == 0);
     inversion_list_destroy(set1);
 
-    set1 = inversion_list_intersection(set, set2, reinterpret_cast<InversionList*>(NULL));
+    set1 = inversion_list_intersection(set, set2,null);
     assert(strcmp(inversion_list_to_string(set1), "[1, 2, 3, 5, 7, 9]") == 0);
 
+    inversion_list_destroy(null);
     inversion_list_destroy(set);
     inversion_list_destroy(set1);
     inversion_list_destroy(set2);
