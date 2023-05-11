bc00dfaea5d7bfb7b8dfbb193c6a86176a3f676d
hou-12 <82287928+hou-12@users.noreply.github.com>
2023-05-12 01:20:18 +0200

    Update test-struct.c

--- test/inversion-list/test-struct.c
+++ test/inversion-list/test-struct.c
@@ -11,11 +11,11 @@
 int main(void) {
   InversionList set;
   assert(sizeof set == 2 * sizeof(unsigned int) + sizeof(size_t));
-  assert((void *)&set.capacity == (void *)&set);
-  assert((void *)&set.support == (void *)&set + sizeof(unsigned int));
-  assert((void *)&set.size == (void *)&set + 2 * sizeof(unsigned int));
-  assert((void *)set.couples == (void *)&set + 2 * sizeof(unsigned int) + sizeof(size_t));
-  assert((void *)&set.couples[0] == (void *)&set + 2 * sizeof(unsigned int) + sizeof(size_t));
-  assert((void *)&set.couples[1] == (void *)&set + 2 * sizeof(unsigned int) + sizeof(size_t) + sizeof(unsigned int));
+  assert(reinterpret_cast<void*>&set.capacity == reinterpret_cast<void*>&set);
+  assert(reinterpret_cast<void*>&set.support == reinterpret_cast<void*>&set + sizeof(unsigned int));
+  assert(reinterpret_cast<void*>&set.size == reinterpret_cast<void*>&set + 2 * sizeof(unsigned int));
+  assert(reinterpret_cast<void*>set.couples == reinterpret_cast<void*>&set + 2 * sizeof(unsigned int) + sizeof(size_t));
+  assert(reinterpret_cast<void*>&set.couples[0] == reinterpret_cast<void*>&set + 2 * sizeof(unsigned int) + sizeof(size_t));
+  assert(reinterpret_cast<void*>&set.couples[1] == reinterpret_cast<void*>&set + 2 * sizeof(unsigned int) + sizeof(size_t) + sizeof(unsigned int));
   return EXIT_SUCCESS;
 }
