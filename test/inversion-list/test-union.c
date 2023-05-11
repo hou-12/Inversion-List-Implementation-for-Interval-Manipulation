64e5cfb6ca3327986e54963a560c7c685bde61e8
hou-12 <82287928+hou-12@users.noreply.github.com>
2023-05-12 01:13:58 +0200

    Update test-union.c

--- test/inversion-list/test-union.c
+++ test/inversion-list/test-union.c
@@ -11,8 +11,6 @@
 #include "inversion-list/inversion-list.inc"
 
 int main(void) {
-  
-    
   inversion_list_init();
   {
     unsigned int i;
@@ -26,13 +24,12 @@ int main(void) {
     InversionList *set2 = inversion_list_create(20, sizeof b / sizeof *b, b);
     InversionList *set3 = inversion_list_create(30, sizeof c / sizeof *c, c);
 
-    set1 = inversion_list_union(set, set2, set3, (InversionList*)NULL);
-    assert(strcmp(inversion_list_to_string(set1),"[1, 2, 3, 5, 7, 9, 10, 12, 23]") == 0);
+    set1 = inversion_list_union(set, set2, set3, (InversionList *)NULL);
+    assert(strcmp(inversion_list_to_string(set1), "[1, 2, 3, 5, 7, 9, 10, 12, 23]") == 0);
     inversion_list_destroy(set1);
 
-    set1 = inversion_list_union(set, set2, (InversionList*)NULL);
-    assert(strcmp(inversion_list_to_string(set1),"[1, 2, 3, 5, 7, 9, 10]") == 0);
-      
+    set1 = inversion_list_union(set, set2, (InversionList *)NULL);
+    assert(strcmp(inversion_list_to_string(set1), "[1, 2, 3, 5, 7, 9, 10]") == 0);
 
     inversion_list_destroy(set);
     inversion_list_destroy(set1);
