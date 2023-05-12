f6bee22e34a423f10de336df561a43b8678c0384
hou-12 <82287928+hou-12@users.noreply.github.com>
2023-05-12 05:03:03 +0200

    Update test-struct.c

--- test/inversion-list/test-struct.c
+++ test/inversion-list/test-struct.c
@@ -8,15 +8,15 @@
 #include "inversion-list/inversion-list.h"
 #include "inversion-list/inversion-list.inc"
 
-// NOLINTNEXTLINE(runtime/references)
+
 int main(void) {
   InversionList set;
-  assert(sizeof set == 2 * sizeof(unsigned int) + sizeof(size_t));
-  assert((void *)&set.capacity == (void *)&set);
-  assert((void *)&set.support == (void *)&set + sizeof(unsigned int));
-  assert((void *)&set.size == (void *)&set + 2 * sizeof(unsigned int));
-  assert((void *)set.couples == (void *)&set + 2 * sizeof(unsigned int) + sizeof(size_t));
-  assert((void *)&set.couples[0] == (void *)&set + 2 * sizeof(unsigned int) + sizeof(size_t));
-  assert((void *)&set.couples[1] == (void *)&set + 2 * sizeof(unsigned int) + sizeof(size_t) + sizeof(unsigned int));
+  assert(sizeof set == 2 * sizeof(unsigned int) + sizeof(size_t));  // NOLINT(build/include)
+  assert((void *)&set.capacity == (void *)&set);  // NOLINT(build/include)
+  assert((void *)&set.support == (void *)&set + sizeof(unsigned int));  // NOLINT(build/include)
+  assert((void *)&set.size == (void *)&set + 2 * sizeof(unsigned int));  // NOLINT(build/include)
+  assert((void *)set.couples == (void *)&set + 2 * sizeof(unsigned int) + sizeof(size_t));  // NOLINT(build/include)
+  assert((void *)&set.couples[0] == (void *)&set + 2 * sizeof(unsigned int) + sizeof(size_t));  // NOLINT(build/include)
+  assert((void *)&set.couples[1] == (void *)&set + 2 * sizeof(unsigned int) + sizeof(size_t) + sizeof(unsigned int));  // NOLINT(build/include)
   return EXIT_SUCCESS;
 }
