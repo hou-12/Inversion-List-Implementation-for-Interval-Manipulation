c589e6a16a49718f4bddc3032abb7ca62c6bda02
hou-12 <82287928+hou-12@users.noreply.github.com>
2023-05-15 02:21:44 +0200

    Update test-equals.c

--- test/inversion-list/test-equals.c
+++ test/inversion-list/test-equals.c
@@ -17,32 +17,36 @@ int main(void) {
     unsigned int b[] = {1, 2, 3, 5, 7, 9, 10};
     unsigned int c[] = {1, 2, 3, 5, 7, 9};
 
-    InversionList *set = inversion_list_create(20, sizeof a / sizeof *a, a);
+    InversionList *set =
+        inversion_list_create(20, sizeof a / sizeof *a, a);
     InversionList *set1 = inversion_list_clone(set);
 
-    InversionList *set2 = inversion_list_create(20, sizeof b / sizeof *b, b);
-    InversionList *set3 = inversion_list_create(20, sizeof c / sizeof *c, c);
+    InversionList *set2 =
+        inversion_list_create(20, sizeof b / sizeof *b, b);
+    InversionList *set3 =
+        inversion_list_create(20, sizeof c / sizeof *c, c);
 
-    //test equal
+    // test equal
     assert(inversion_list_equal(set, set1));
+    assert(inversion_list_equal(set, set));
     assert(!inversion_list_equal(set, set2));
     assert(!inversion_list_equal(set, set3));
     assert(!inversion_list_equal(set2, set3));
 
-    //test not equal
+    // test not equal
     assert(!inversion_list_not_equal(set, set1));
+    assert(!inversion_list_not_equal(set, set));
     assert(inversion_list_not_equal(set, set2));
     assert(inversion_list_not_equal(set, set3));
     assert(inversion_list_not_equal(set2, set3));
 
-    //test less
+    // test less
 
-    //test less-equal
+    // test less-equal
 
-    //test greater
+    // test greater
 
     // test greater-equal
-
 
     inversion_list_destroy(set);
     inversion_list_destroy(set1);
