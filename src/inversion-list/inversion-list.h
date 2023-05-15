97d1aea3b5536567a7f02e453ec39aa8da61f101
hou-12 <82287928+hou-12@users.noreply.github.com>
2023-05-15 03:40:56 +0200

    Update inversion-list.h

--- src/inversion-list/inversion-list.h
+++ src/inversion-list/inversion-list.h
@@ -113,74 +113,274 @@ extern InversionList *inversion_list_complement(const InversionList *set);
  */
 extern const char *inversion_list_to_string(const InversionList *set);
 
+/**
+ * Compare two sets to know if they are equal.
+ *
+ * @param set1 a set that will be compared
+ * @param set2 a set that will be compared
+ *
+ * @return true if @p set1 equal @p set2 or false if not equal
+ *
+ */
 extern bool inversion_list_equal(const InversionList *set1,
                                  const InversionList *set2);
+
+/**
+ * Compare two sets to know if they are not equal.
+ *
+ * @param set1 a set that will be compared
+ * @param set2 a set that will be compared
+ *
+ * @return true if @p set1 not equal @p set2 or false if equal
+ *
+ */
 extern bool inversion_list_not_equal(const InversionList *set1,
                                      const InversionList *set2);
 
+/**
+ * Compare two sets to know if one of them is less than the other.
+ *
+ * @param set1 a set that will be compared
+ * @param set2 a set that will be compared
+ *
+ * @return true if @p set1 less than @p set2 or false if not
+ *
+ */
 extern bool inversion_list_less(const InversionList *set1,
                                 const InversionList *set2);
 
+/**
+ * Compare two sets to know if one of them is less or equal than the other.
+ *
+ * @param set1 a set that will be compared
+ * @param set2 a set that will be compared
+ *
+ * @return true if @p set1 less or equal than @p set2 or false if not
+ *
+ */
 extern bool inversion_list_less_equal(const InversionList *set1,
                                       const InversionList *set2);
 
+/**
+ * Compare two sets to know if one of them is greater than the other.
+ *
+ * @param set1 a set that will be compared
+ * @param set2 a set that will be compared
+ *
+ * @return true if @p set1 greater than @p set2 or false if not
+ *
+ */
 extern bool inversion_list_greater(const InversionList *set1,
                                    const InversionList *set2);
 
+/**
+ * Compare two sets to know if one of them is greater or equal than the
+ * other.
+ *
+ * @param set1 a set that will be compared
+ * @param set2 a set that will be compared
+ *
+ * @return true if @p set1 greater or equal than @p set2 or false if not
+ *
+ */
 extern bool inversion_list_greater_equal(const InversionList *set1,
                                          const InversionList *set2);
 
+/**
+ * Compare two sets to know if they are disjoint.
+ *
+ * @param set1 a set that will be compared
+ * @param set2 a set that will be compared
+ *
+ * @return true if @p set1 and @p set2 are disjoint or false if not
+ *
+ */
 extern bool inversion_list_disjoint(const InversionList *set1,
                                     const InversionList *set2);
 
+/**
+ * Create a set that is the symmetric difference of two sets.
+ *
+ * @param set1 a set that will be compared
+ * @param set2 a set that will be compared
+ *
+ * @return the symmetric difference of @p set1 and @p set2
+ *
+ */
 extern InversionList *inversion_list_symmetric_difference(
     const InversionList *set1, const InversionList *set2);
 
+/**
+ * Create a set that is the union of n sets.
+ *
+ * @param set1 a set that will be compared
+ *
+ * @return the union of @p set1 and n @p set
+ *
+ * @note the last element must be `NULL`
+ */
 extern InversionList *inversion_list_union(const InversionList *set, ...);
 
+/**
+ * Create a set that is the intersection of n sets.
+ *
+ * @param set1 a set that will be compared
+ *
+ * @return the intersection of @p set1 and n @p set
+ *
+ * @note the last element must be `NULL`
+ */
 extern InversionList *inversion_list_intersection(const InversionList *set,
                                                   ...);
 
+/**
+ * Create a set that is the difference of n sets.
+ *
+ * @param set1 a set that will be compared
+ *
+ * @return the difference of @p set1 and n @p set
+ *
+ * @note the last element must be `NULL`
+ */
 extern InversionList *inversion_list_difference(const InversionList *set1,
                                                 ...);
 
+/**
+ * Create an iterator over the integers of an inversion list.
+ *
+ * @param set1
+ *
+ * @return a new iterator an iterator over the integers of @p set1.
+ *
+ */
 extern InversionListIterator *inversion_list_iterator_create(
     const InversionList *set);
 
+/**
+ * Destroy an iterator over the integers of an inversion list.
+ *
+ * @param iterator to be destroy
+ *
+ */
 extern void inversion_list_iterator_destroy(
     InversionListIterator *iterator);
 
+/**
+ * Implementation of the index.
+ *
+ * @param iterator the iterator that will have its index implemented
+ *
+ * @return @p iterator with his index implemented.
+ *
+ */
 extern InversionListIterator *inversion_list_iterator_next(
     InversionListIterator *iterator);
 
+/**
+ * Reset of the index.
+ *
+ * @param iterator the iterator that will have its index reset
+ *
+ * @return @p iterator with his index rest.
+ *
+ */
 extern InversionListIterator *inversion_list_iterator_rewind(
     InversionListIterator *iterator);
 
+/**
+ * Check of the iterator is stii valid.
+ *
+ * @param iterator the iterator that will be checked
+ *
+ * @return true if iterator is valid, false otherwise.
+ *
+ */
 extern bool inversion_list_iterator_valid(
     const InversionListIterator *iterator);
 
+/**
+ * Get the current member of the iterator.
+ *
+ * @param iterator the iterator that will give his current member
+ *
+ * @return the current membre of @p iterator
+ *
+ */
 extern unsigned int inversion_list_iterator_get(
     InversionListIterator *iterator);
 
-/****COUPLE ITERATOR******/
+/**
+ * Create an iterator over the couples of an inversion list.
+ *
+ * @param set
+ *
+ * @return a new iterator over the couples of @p set1.
+ *
+ */
 extern InversionListCoupleIterator *inversion_list_couple_iterator_create(
     const InversionList *set);
 
+/**
+ * Destroy an iterator over the couples of an inversion list.
+ *
+ * @param iterator to be destroy
+ *
+ */
 extern void inversion_list_couple_iterator_destroy(
     InversionListCoupleIterator *iterator);
 
+/**
+ * Implementation of the index.
+ *
+ * @param iterator the iterator that will have its index implemented
+ *
+ * @return @p iterator with his index implemented.
+ *
+ */
 extern InversionListCoupleIterator *inversion_list_couple_iterator_next(
     InversionListCoupleIterator *iterator);
 
+/**
+ * Reset of the index.
+ *
+ * @param iterator the iterator that will have its index reset
+ *
+ * @return @p iterator with his index rest.
+ *
+ */
 extern InversionListCoupleIterator *inversion_list_couple_iterator_rewind(
     InversionListCoupleIterator *iterator);
 
+/**
+ * Check of the iterator is stii valid.
+ *
+ * @param iterator the iterator that will be checked
+ *
+ * @return true if iterator is valid, false otherwise.
+ *
+ */
 extern bool inversion_list_iterator_couple_valid(
     const InversionListCoupleIterator *iterator);
 
+/**
+ * Get the current inferior member of the current couple.
+ *
+ * @param iterator the iterator that will give his current inferior member
+ *
+ * @return the current inferior membre of @p iterator
+ *
+ */
 extern unsigned int inversion_list_couple_iterator_get_inf(
     const InversionListCoupleIterator *iterator);
 
+/**
+ * Get the current superior member of the current couple.
+ *
+ * @param iterator the iterator that will give his current superior member
+ *
+ * @return the current superior membre of @p iterator
+ *
+ */
 extern unsigned int inversion_list_couple_iterator_get_sup(
     const InversionListCoupleIterator *iterator);
 #endif  // INVERSION_LIST_INVERSION_LIST_H_
